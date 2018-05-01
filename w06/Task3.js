e13361f5f9862c0e090bbf558946ca6a83b58f2b
stinarchan <38451520+stinarchan@users.noreply.github.com>
2018-05-01 14:44:22 +0900

    Update Task3.js

--- w06/Task3.js
+++ w06/Task3.js
@@ -3,45 +3,64 @@ function main()
     var width = 500;
     var height = 500;
 
-    var scene = new THREE.Scene();
+    var scene_Gouraud_Lambertian = new THREE.Scene();
+    var scene_Gouraud_Phong = new THREE.Scene();
 
-    var fov = 45;
+    var fov = 50;
     var aspect = width / height;
     var near = 1;
     var far = 1000;
     var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
     camera.position.set( 0, 0, 5 );
-    scene.add( camera );
+    scene_Gouraud_Lambertian.add( camera );
+    scene_Gouraud_Phong.add( camera);
 
     var light = new THREE.PointLight();
     light.position.set( 5, 5, 5 );
-    scene.add( light );
+    scene_Gouraud_Lambertian.add( light );
+    scene_Gouraud_Phong.add(light);
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( width, height );
-    document.body.appendChild( renderer.domElement );
+    var renderer_Gouraud_Lambertian = new THREE.WebGLRenderer();
+    renderer_Gouraud_Lambertian.setSize( width, height );
+    document.body.appendChild( renderer_Gouraud_Lambertian.domElement );
+    var renderer_Gouraud_Phong = new THREE.WebGLRenderer();
+    renderer_Gouraud_Phong.setSize( 500, 500 );
+    document.body.appendChild (renderer_Gouraud_Phong.domElement );
+
+    var p1 = document.createElement('p');
+    p1.innerHTML = "The left is using Gouraud reflection."
+    +"<br>"
+    +"The light is using Phong reflection.";
+    document.body.appendChild( p1 );
+
 
     var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
-    var material = new THREE.ShaderMaterial({
-        vertexColors: THREE.VertexColors,
-        vertexShader: document.getElementById('shader.vert').text,
-        fragmentShader: document.getElementById('shader.frag').text,
-                    uniforms: {
-        light_position: {type: 'v3',value: light.position},
-        camera_position: {type: 'v3',value: camera.position}
-    }
+    var material_Gouraud_Lambertian = new THREE.ShaderMaterial({
+      vertexColors: THREE.VertexColors,
+      vertexShader: document.getElementById('Gouraud.vert').text,
+      fragmentShader:document.getElementById('Gouraud.frag').text
     });
 
-    var torus_knot = new THREE.Mesh( geometry, material );
-    scene.add( torus_knot );
+    var material_Gouraud_Phong = new THREE.ShaderMaterial({
+      vertexColors: THREE.VertexColors,
+      vertexShader: document.getElementById('Phong.vert').text,
+      fragmentShader:document.getElementById('Phong.frag').text
+    });
 
+    var torus_knot_Gouraud_Lambertian = new THREE.Mesh( geometry, material_Gouraud_Lambertian );
+    var torus_knot_Gouraud_Phong = new THREE.Mesh( geometry, material_Gouraud_Phong );
+    scene_Gouraud_Lambertian.add( torus_knot_Gouraud_Lambertian );
+    scene_Gouraud_Phong.add( torus_knot_Gouraud_Phong );
     loop();
 
     function loop()
     {
         requestAnimationFrame( loop );
-        torus_knot.rotation.x += 0.01;
-        torus_knot.rotation.y += 0.01;
-        renderer.render( scene, camera );
+        torus_knot_Gouraud_Lambertian.rotation.x += 0.01;
+        torus_knot_Gouraud_Lambertian.rotation.y += 0.01;
+        torus_knot_Gouraud_Phong.rotation.x += 0.01;
+        torus_knot_Gouraud_Phong.rotation.y += 0.01;
+        renderer_Gouraud_Lambertian.render( scene_Gouraud_Lambertian, camera );
+        renderer_Gouraud_Phong.render(scene_Gouraud_Phong, camera);
     }
 }
