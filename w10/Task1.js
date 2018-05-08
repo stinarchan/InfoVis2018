62ef8daebc61b325f31e20156a62d3d39213923c
stinarchan <38451520+stinarchan@users.noreply.github.com>
2018-05-08 14:19:22 +0900

    Create Task1.js

---
+++ w10/Task1.js
@@ -0,0 +1,22 @@
+function main()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+  
+    material.color = new THREE.Color( "white" );
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+    screen.loop();
+}
